5018e833a86b522132bfcb19c96f3629a9946c46
1kweku <kwekuplusp@gmail.com>
2022-09-07 19:52:47 -0400

    readme added

--- Develop/script.js
+++ Develop/script.js
@@ -10,9 +10,8 @@ function randomInt(min, max) {
     min = 0
   }
 }
-
+//function created to randomize data and floor value
 function getRandomItem(list){
-  //return list[randomInt(0, list.length - 1)]
   return list[Math.floor(Math.random()*list.length - 1)]
 }
 
@@ -24,17 +23,17 @@ function generatePassword() {
     window.alert("Password length must be between 8 and 128.")
     return
   }
-
+// variables created for prompts
   var userNumbers = window.confirm('Do you want numbers in your password?')
   var userSymbols = window.confirm('Do you want symbols in your password?')
   var userUppercase = window.confirm('Do you want uppercase letters in your password?')
   var userLowercase = window.confirm('Do you want lowercase letters in your password?')
-
+// variables created to store numbers, symbols and letters
   var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
   var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "=", "+"]
   var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
   var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
+// characterData variable created for user choices to push into
   var characterData = []
 
   if (userNumbers === true) {
@@ -54,7 +53,7 @@ function generatePassword() {
   }
 console.log(characterData)
   var generatedPassword = ""
-
+// variables created to log character data and return information
   for (var i = 0; i < userInput; i++) {
     var randomItem = getRandomItem(characterData)
     var randomChar = getRandomItem(randomItem)
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Password Generator Starter Code
+# Introduction
+This purpose of this site is to generate a randomized password, which can be copied and used to create a secure login. User has the option of choosing if this password will contain uppercase and lowercase letters, as well as numbers and symbols by interacting with prompts.
+
+## Screenshot + link
+![](Screenshot/Screen%20Shot%202022-09-07%20at%207.45.08%20PM.png)
+
